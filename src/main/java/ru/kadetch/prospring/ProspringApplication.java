924ff71ebce5955ed69728ae1a515515097cbed4
KadetCh <79017988+kadetch@users.noreply.github.com>
2022-10-13 23:44:48 +0300

    initialization of the project

---
+++ src/main/java/ru/kadetch/prospring/ProspringApplication.java
@@ -0,0 +1,13 @@
+package ru.kadetch.prospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kadetch/prospring/ProspringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kadetch.prospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
