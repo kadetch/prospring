950e1613864768731d95085f8af7cab9eadbeb8d
KadetCh <kadetch@yandex.ru>
2022-10-14 23:08:46 +0300

    Implementation of the InitializingBean interface

--- src/main/java/ru/kadetch/prospring/ch4/SingerWithInterface.java
+++ src/main/java/ru/kadetch/prospring/ch4/SingerWithInterface.java
@@ -1,12 +1,13 @@
 package ru.kadetch.prospring.ch4;
 
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class Singer {
-    private static final String DEFAULT_NAME = "Eric Clapton";
+public class SingerWithInterface implements InitializingBean {
 
+    private static final String DEFAULT_NAME = "Eric Clapton";
     private String name;
     private int age = Integer.MIN_VALUE;
 
@@ -18,9 +19,9 @@ public class Singer {
         this.age = age;
     }
 
-    public void init() {
+    @Override
+    public void afterPropertiesSet() throws Exception {
         System.out.println("Initializing bean");
-
         if (name == null) {
             System.out.println("Using default name");
             name = DEFAULT_NAME;
@@ -28,7 +29,8 @@ public class Singer {
 
         if (age == Integer.MIN_VALUE) {
             throw new IllegalArgumentException("You must set "
-                    + "the age property of any beans of type Singer.class");
+                    + "the age property of any beans of type "
+                    + SingerWithInterface.class);
         }
     }
 
@@ -36,27 +38,28 @@ public class Singer {
         return "\tName: " + name + "\n\tAge: " + age;
     }
 
-//    public static void main(String... args) {
-//        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-//        ctx.load("classpath:spring/app-context-xml2.xml");
-//        ctx.refresh();
-//
-//        getBean("singerOne", ctx);
-//        getBean("singerTwo", ctx);
-//        getBean("singerThree", ctx);
-//
-//        ctx.close();
-//    }
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-xml.xml");
+        ctx.refresh();
 
-    public static Singer getBean(String beanName, ApplicationContext ctx) {
+        getBean("singerOne", ctx);
+        getBean("singerTwo", ctx);
+        getBean("singerThree", ctx);
+
+        ctx.close();
+    }
+
+    public static SingerWithInterface getBean(String beanName, ApplicationContext ctx) {
         try {
-            Singer bean = (Singer) ctx.getBean(beanName);
+            SingerWithInterface bean = (SingerWithInterface) ctx.getBean(beanName);
             System.out.println(bean);
             return bean;
         } catch (BeanCreationException ex) {
             System.out.println("An error occured in bean "
-                    + "configuration: " + ex.getMessage());
+                    + "configuretion: " + ex.getMessage());
             return null;
         }
+
     }
 }
