c306f2f74d084cc2c55a4695d37c969c4e160c01
KadetCh <kadetch@yandex.ru>
2022-10-14 23:08:46 +0300

    Implementation of the InitializingBean interface

---
+++ src/main/java/ru/kadetch/prospring/ch4/Application.java
@@ -0,0 +1,31 @@
+package ru.kadetch.prospring.ch4;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String... args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-xml.xml");
+        ctx.refresh();
+
+        getBean("singerOne", ctx);
+        getBean("singerTwo", ctx);
+        getBean("singerThree", ctx);
+
+        ctx.close();
+    }
+
+    public static SingerWithInterface getBean(String beanName, ApplicationContext ctx) {
+        try {
+            SingerWithInterface bean = (SingerWithInterface) ctx.getBean(beanName);
+            System.out.println(bean);
+            return bean;
+        } catch (BeanCreationException ex) {
+            System.out.println("An error occured in bean "
+                    + "configuretion: " + ex.getMessage());
+            return null;
+        }
+    }
+}
--- src/main/java/ru/kadetch/prospring/ch4/SingerWithInterface.java
+++ src/main/java/ru/kadetch/prospring/ch4/SingerWithInterface.java
@@ -1,12 +1,13 @@
 package ru.kadetch.prospring.ch4;
 
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class Singer {
-    private static final String DEFAULT_NAME = "Eric Clapton";
+public class SingerWithInterface implements InitializingBean {
 
+    private static final String DEFAULT_NAME = "Eric Clapton";
     private String name;
     private int age = Integer.MIN_VALUE;
 
@@ -18,9 +19,9 @@ public class Singer {
         this.age = age;
     }
 
-    public void init() {
+    @Override
+    public void afterPropertiesSet() throws Exception {
         System.out.println("Initializing bean");
-
         if (name == null) {
             System.out.println("Using default name");
             name = DEFAULT_NAME;
@@ -28,7 +29,8 @@ public class Singer {
 
         if (age == Integer.MIN_VALUE) {
             throw new IllegalArgumentException("You must set "
-                    + "the age property of any beans of type Singer.class");
+                    + "the age property of any beans of type "
+                    + SingerWithInterface.class);
         }
     }
 
