29d5f8be7779da203a673d01be2de3240f54d45a
KadetCh <kadetch@yandex.ru>
2022-10-17 00:42:52 +0300

    Declaring an initialization method using the @Bean annotation

--- src/main/java/ru/kadetch/prospring/ch4/Application.java
+++ src/main/java/ru/kadetch/prospring/ch4/Application.java
@@ -2,14 +2,13 @@ package ru.kadetch.prospring.ch4;
 
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Application {
     public static void main(String... args) {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-//        ctx.load("classpath:spring/app-context-xml.xml");
-        ctx.load("classpath:spring/app-context-annotation.xml");
-        ctx.refresh();
+        GenericApplicationContext ctx = new AnnotationConfigApplicationContext(SingerConfig.class);
 
         getBean("singerOne", ctx);
         getBean("singerTwo", ctx);
@@ -18,9 +17,9 @@ public class Application {
         ctx.close();
     }
 
-    public static SingerWithJSR250 getBean(String beanName, ApplicationContext ctx) {
+    public static Singer getBean(String beanName, ApplicationContext ctx) {
         try {
-            SingerWithJSR250 bean = (SingerWithJSR250) ctx.getBean(beanName);
+            Singer bean = (Singer) ctx.getBean(beanName);
             System.out.println(bean);
             return bean;
         } catch (BeanCreationException ex) {
